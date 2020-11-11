6b87a0890ed368a86e52b04b0dc5d1784f06ba1b
mario-lafuente <info@mariolafuente.com>
2020-11-11 15:40:27 -0600

    removing api file

--- src/components/api/googleTraslate.js
+++
@@ -1,4 +0,0 @@
-export const googleLngApi = 'https://translation.googleapis.com/language/translate/v2'
-
-export const key = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
